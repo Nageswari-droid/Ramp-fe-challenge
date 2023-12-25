a8c0fe87d7aea093e3a18f7f94fd896355cb67e2
Nageswari-droid <nachasenthil@gmail.com>
2023-12-25 01:53:06 -0700

    [Bug 7] | Refactor

--- src/hooks/useCustomFetch.ts
+++ src/hooks/useCustomFetch.ts
@@ -2,7 +2,8 @@ import { useCallback, useContext } from "react"
 import { AppContext } from "../utils/context"
 import { fakeFetch, RegisteredEndpoints } from "../utils/fetch"
 import { useWrappedRequest } from "./useWrappedRequest"
-import { PaginatedResponse, SetTransactionApprovalParams, Transaction } from "src/utils/types"
+import { SetTransactionApprovalParams } from "../utils/types"
+import { updateTransactionApprovalState } from "../utils/updateTransaction"
 
 export function useCustomFetch() {
   const { cache } = useContext(AppContext)
@@ -79,51 +80,4 @@ export function useCustomFetch() {
 
 function getCacheKey(endpoint: RegisteredEndpoints, params?: object) {
   return `${endpoint}${params ? `@${JSON.stringify(params)}` : ""}`
-}
-
-function updateTransactionApprovalState(
-  cache: React.MutableRefObject<Map<string, string>>,
-  params: {}
-) {
-  const cacheKeys = Array.from(cache.current.keys())
-  let paginatedTransactionKey: (string | undefined)[] = []
-  let transactionsByEmployeeKey:(string | undefined)[] = []
-
-  for (const key of cacheKeys) {
-    if (key.match("paginatedTransactions") !== null || key.match("transactionsByEmployee")) {
-      paginatedTransactionKey.push(key.match("paginatedTransactions")?.input)
-      transactionsByEmployeeKey.push(key.match("transactionsByEmployee")?.input)
-    }
-  }
-
-  updateCache(paginatedTransactionKey, cache, params, false)
-  updateCache(transactionsByEmployeeKey, cache, params, true)
-}
-
-function updateCache(
-  cacheKey: (string | undefined)[],
-  cache: React.MutableRefObject<Map<string, string>>,
-  params: {},
-  isByEmployeeId: Boolean
-) {
-  cacheKey.forEach((key) => {
-    if (key !== null && key !== undefined) {
-      let cacheResponse = cache.current.get(key)
-      if (cacheResponse !== undefined) {
-        let data = JSON.parse(cacheResponse)
-        let transactions = isByEmployeeId
-          ? (data as Transaction[])
-          : (data as PaginatedResponse<Transaction[]>).data
-        if (transactions) {
-          transactions.forEach((element) => {
-            let transactionApprovalParams = params as SetTransactionApprovalParams
-            if (transactionApprovalParams.transactionId === element.id) {
-              element.approved = transactionApprovalParams.value
-            }
-          })
-          cache.current.set(key, JSON.stringify(data))
-        }
-      }
-    }
-  })
 }
