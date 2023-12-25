f87c67e1990a1c5a9750b1736215a095c65fd824
Nageswari-droid <nachasenthil@gmail.com>
2023-12-25 01:42:55 -0700

    [Bug 7] | Refactor

--- src/hooks/useCustomFetch.ts
+++ src/hooks/useCustomFetch.ts
@@ -74,94 +74,56 @@ export function useCustomFetch() {
     [cache]
   )
 
-  function updateTransactionApprovalState(
-    cache: React.MutableRefObject<Map<string, string>>,
-    params: {}
-  ) {
-    const cacheKeys = Array.from(cache.current.keys())
-    let paginatedTransactionKey = []
-    let transactionsByEmployeeKey = []
-
-    for (const key of cacheKeys) {
-      if (key.match("paginatedTransactions") !== null || key.match("transactionsByEmployee")) {
-        paginatedTransactionKey.push(key.match("paginatedTransactions")?.input)
-        transactionsByEmployeeKey.push(key.match("transactionsByEmployee")?.input)
-      }
-    }
-
-    transactionsByEmployeeKey.forEach((key) => {
-      if (key !== null && key !== undefined) {
-        let cacheResponse = cache.current.get(key)
-
-        if (cacheResponse !== undefined) {
-          let data = JSON.parse(cacheResponse)
-          if (data as Transaction[]) {
-            let transactionData = data as Transaction[]
-            transactionData.forEach((element) => {
-              let transactionApprovalParams = params as SetTransactionApprovalParams
-              if (transactionApprovalParams.transactionId === element.id) {
-                element.approved = transactionApprovalParams.value
-              }
-            })
-            cache.current.set(key, JSON.stringify(data))
-          }
-        }
-      }
-    })
-
-    paginatedTransactionKey.forEach((key) => {
-      if (key !== null && key !== undefined) {
-        let cacheResponse = cache.current.get(key)
-
-        if (cacheResponse !== undefined) {
-          let data = JSON.parse(cacheResponse)
-          if (data as PaginatedResponse<Transaction[]>) {
-            let paginatedData = (data as PaginatedResponse<Transaction[]>).data
-            paginatedData.forEach((element) => {
-              let transactionApprovalParams = params as SetTransactionApprovalParams
-              if (transactionApprovalParams.transactionId === element.id) {
-                element.approved = transactionApprovalParams.value
-              }
-            })
-            cache.current.set(key, JSON.stringify(data))
-          }
-        }
-      }
-    })
-  }
-
-  // function updateTransactionApprovalCache(
-  //   cacheKey: [],
-  //   cache: React.MutableRefObject<Map<string, string>>,
-  //   params: {},
-  //   isByEmployeeId: Boolean
-  // ) {
-  //   cacheKey.forEach((key) => {
-  //     if (key !== null && key !== undefined) {
-  //       let cacheResponse = cache.current.get(key)
-
-  //       if (cacheResponse !== undefined) {
-  //         let data = JSON.parse(cacheResponse)
-  //         let transactions = isByEmployeeId
-  //           ? (data as Transaction[])
-  //           : (data as PaginatedResponse<Transaction[]>).data
-  //         if (transactions) {
-  //           transactions.forEach((element) => {
-  //             let transactionApprovalParams = params as SetTransactionApprovalParams
-  //             if (transactionApprovalParams.transactionId === element.id) {
-  //               element.approved = transactionApprovalParams.value
-  //             }
-  //           })
-  //           cache.current.set(key, JSON.stringify(data))
-  //         }
-  //       }
-  //     }
-  //   })
-  // }
-
   return { fetchWithCache, fetchWithoutCache, clearCache, clearCacheByEndpoint, loading }
 }
 
 function getCacheKey(endpoint: RegisteredEndpoints, params?: object) {
   return `${endpoint}${params ? `@${JSON.stringify(params)}` : ""}`
+}
+
+function updateTransactionApprovalState(
+  cache: React.MutableRefObject<Map<string, string>>,
+  params: {}
+) {
+  const cacheKeys = Array.from(cache.current.keys())
+  let paginatedTransactionKey: (string | undefined)[] = []
+  let transactionsByEmployeeKey:(string | undefined)[] = []
+
+  for (const key of cacheKeys) {
+    if (key.match("paginatedTransactions") !== null || key.match("transactionsByEmployee")) {
+      paginatedTransactionKey.push(key.match("paginatedTransactions")?.input)
+      transactionsByEmployeeKey.push(key.match("transactionsByEmployee")?.input)
+    }
+  }
+
+  updateCache(paginatedTransactionKey, cache, params, false)
+  updateCache(transactionsByEmployeeKey, cache, params, true)
+}
+
+function updateCache(
+  cacheKey: (string | undefined)[],
+  cache: React.MutableRefObject<Map<string, string>>,
+  params: {},
+  isByEmployeeId: Boolean
+) {
+  cacheKey.forEach((key) => {
+    if (key !== null && key !== undefined) {
+      let cacheResponse = cache.current.get(key)
+      if (cacheResponse !== undefined) {
+        let data = JSON.parse(cacheResponse)
+        let transactions = isByEmployeeId
+          ? (data as Transaction[])
+          : (data as PaginatedResponse<Transaction[]>).data
+        if (transactions) {
+          transactions.forEach((element) => {
+            let transactionApprovalParams = params as SetTransactionApprovalParams
+            if (transactionApprovalParams.transactionId === element.id) {
+              element.approved = transactionApprovalParams.value
+            }
+          })
+          cache.current.set(key, JSON.stringify(data))
+        }
+      }
+    }
+  })
 }
